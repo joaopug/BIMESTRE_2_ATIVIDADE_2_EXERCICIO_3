f928ed26d62f195984f0bf2243590a6689f2ee83
joaopug <charmandercharmandesco@gmail.com>
2024-11-14 14:24:48 -0300

    Algumas variáveis foram iniciadas globalmente, além de melhorias da exibição das mensagens. Botão "Limpar adicionado".

--- src/Notas_e_Media.java
+++ src/Notas_e_Media.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 public class Notas_e_Media {
     private JTextArea txtAreaNotas;
@@ -9,40 +8,66 @@ public class Notas_e_Media {
     private JButton btnAdicionar;
     private JButton btnCalcularMedia;
     private JPanel Aplicativo_de_Notas;
+    private JTextArea txtAreaAvaliacao;
+    private JButton btnLimpar;
     private JLabel lblAvaliacao;
 
 
-    private Double memes = 0.0;
+    private Double notasSomadas = 0.0;
     private int i = 0;
-    ArrayList notas = new ArrayList();
+    private Double media = 0.0;
+    private Double notaRecebida = 0.0;
 
     public Notas_e_Media() {
-    btnAdicionar.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            Double jorge = Double.valueOf(txtNota.getText());
-            txtAreaNotas.append(jorge + "\n");
-            memes = jorge + memes;
-            i++;
-        }
-    });
-    btnCalcularMedia.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            Double media = memes / i;
-            if (media >= 70) {
-                lblAvaliacao.setText("Aprovado" + media);
-            } else {
-                lblAvaliacao.setText("Reprovado" + media);
+        btnAdicionar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    notaRecebida = Double.valueOf(txtNota.getText());
+                    if (notaRecebida >= 0) {
+                        notasSomadas = notaRecebida + notasSomadas;
+                        i++;
+                        txtAreaNotas.append(i + "ª nota: " + notaRecebida + "\n");
+                    } else {
+                        txtAreaAvaliacao.setText("Erro");
+                    }
+                } catch (NumberFormatException ex) {
+                    txtAreaAvaliacao.setText("Erro: Caractere inválido!");
+                }
             }
-        }
-    });
+        });
+        btnCalcularMedia.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (i > 0) {
+                    media = notasSomadas / i;
+                    if (media >= 7) {
+                        txtAreaAvaliacao.setText("Aprovado\n" + "Média: " + media);
+                    } else {
+                        txtAreaAvaliacao.setText("Reprovado\n" + "Média: " + media);
+                    }
+                } else {
+                    txtAreaAvaliacao.setText("Nenhuma nota adicionada.");
+                }
+            }
+        });
+        btnLimpar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                i = 0;
+                media = 0.0;
+                notasSomadas = 0.0;
+                txtAreaAvaliacao.setText("");
+                txtAreaNotas.setText("");
+                txtNota.setText("");
+            }
+        });
     }
 
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Notas_e_Media");
-        frame.setSize(600, 400);
+        frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setContentPane((new Notas_e_Media()).Aplicativo_de_Notas);
         frame.setVisible(true);
