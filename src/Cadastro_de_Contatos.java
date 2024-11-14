8cb9a5093e507e42858512a1a12b8e406409179a
joaopug <charmandercharmandesco@gmail.com>
2024-11-14 16:58:25 -0300

    App de "cadastro de contatos" e exibição através deles com JList. Possui verificação dos campos "Telefone" e "Email", também há a função "Deletar Contato".

---
+++ src/Cadastro_de_Contatos.java
@@ -0,0 +1,121 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class Cadastro_de_Contatos {
+    private JPanel Cadastro_de_Contatos;
+    private JTextField txtNome;
+    private JTextField txtTelefone;
+    private JTextField txtEmail;
+    private JButton btnAdicionarContato;
+    private JList<String> listContatos;
+    private JLabel lblNome;
+    private JLabel lblEmail;
+    private JLabel lblTelefone;
+    private JLabel lblListaContatos;
+    private JLabel lblStatus;
+    private JButton btnDeletarContato;
+    private JTextArea txtAreaStatus;
+
+    //Variável contadora
+    private int i = 0;
+    //Variável para armazenar a String dentro de txtNome
+    private String nome = "";
+    //Variável para armazenar a String dentro de txtEmail
+    private String email = "";
+    //Variável para armazenar a String dentro de txtTelefone
+    private String telefone = "";
+
+    //Variável para armazenar as Strings "nome", "email" e "telefone"
+    private String contato = "";
+
+    //Variável que armazena o número de qual elemento da lista está selecionado
+    private int indexSelecionado = 0;
+
+    //Criação de uma ArrayList para armazenar as Strings "contato"
+    ArrayList<String> contatos = new ArrayList<>();
+
+    //Cria uma instância da "DefaultListModel", com o nome "modeloContatos", que será utilizada como o modelo de dados para a JList
+    //Ela é o modelo padrão de lista, como o próprio nome já diz
+    //Aparentemente ela é melhor de manipular dados que são exibidos na JList. Ela consegue exibir, remover ou editar os dados de forma dinâmica.
+    DefaultListModel<String> modeloContatos = new DefaultListModel<>();
+
+    public Cadastro_de_Contatos() {
+        //Setando o modelo que será utilizado na "listContatos" como sendo o "modeloContatos", ou seja, a DefaultListModel
+        listContatos.setModel(modeloContatos);
+
+        btnAdicionarContato.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Pegando que foi escrito em cada campo e colocando em sua rescectica variável
+                nome = txtNome.getText();
+                telefone = txtTelefone.getText();
+                email = txtEmail.getText();
+
+                //Verificação dos dados adquiridos e exibição de erros
+                //
+                //Verificações:
+                //Se há um campo vazio
+                //Se o campo "Telefone" tem 11 caracteres
+                //Se o campo "Telefone" tem somente números
+                //Se o campo "Email" possui uma das strings (domínios) declaradas na verificalção
+                if (nome.isEmpty() || email.isEmpty() || telefone.isEmpty()) {
+                    txtAreaStatus.setText("Preencha todos os campos!");
+                } else if (!telefone.matches("[0-9]+")) {
+                    txtAreaStatus.setText("O telefone deve conter apenas números!");
+                } else if (telefone.length() < 11 || telefone.length() > 12) {
+                    txtAreaStatus.setText("O número de telefone deve ter 11 números!" + "\nExemplo: " + "\n46987539732");
+                } else if (!(email.contains("@gmail.com") || email.contains("@hotmail.com") || email.contains("@outlook.com") || email.contains("@yahoo.com"))) {
+                    txtAreaStatus.setText("Email inválido.");
+                } else {
+                    //Adiciona +1 à variável contadora
+                    i++;
+
+                    //Coloca as Strings "nome", "email" e "telefone na String "contato"
+                    contato = ("Contato " + i + " | Nome: " + nome + " | Telefone: " + telefone + " | Email: " + email);
+                    //Adiciona a String "contato" na ArrayList "contatos"
+                    contatos.add(contato);
+
+                    //Adiciona a String "contato" na ListModel "modeloContatos"
+                    modeloContatos.addElement(contato);
+
+                    //Limpa os campos de cadastro
+                    txtNome.setText("");
+                    txtTelefone.setText("");
+                    txtEmail.setText("");
+
+                    //Escreve a mensagem de sucesso no "txtAreaStatus"
+                    txtAreaStatus.setText("Cadastrado com sucesso!");
+                }
+            }
+        });
+        btnDeletarContato.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Define que o valor da variável int "indexSelecionado" será aquele do qual o campo na JList "listContatos" estiver selecionado
+                //Resumindo, se o campo 0 estiver selecionado, esse será o valor do indexSelecionado
+                indexSelecionado = listContatos.getSelectedIndex();
+
+                //Verifica se o valor do "indexSelecionado" é diferente de -1, ou seja, se há algum campo da "listContatos" selecionado
+                //Caso não ele pedirá para que você selecione
+                //Caso sim, ele irá remover o elemento relacionado ao valor do "indexSelecionado" do "modeloContatos" e consequentemente da JList
+                if (indexSelecionado != -1) {
+                    modeloContatos.removeElementAt(indexSelecionado);
+
+                    txtAreaStatus.setText("Contato removido com sucesso!");
+                } else {
+                    txtAreaStatus.setText("Selecione um contato para deletar.");
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Cadastro de Contatos");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(new Cadastro_de_Contatos().Cadastro_de_Contatos);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
